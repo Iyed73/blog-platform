87515aceea75373ab67c6c1af79393431372488d
iyed <73111142+Iyed73@users.noreply.github.com>
2025-05-08 23:56:32 +0100

    Create core library and refactor configuration

--- apps/blog-api/src/app/app.module.ts
+++ apps/blog-api/src/app/app.module.ts
@@ -1,44 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule, ConfigType } from '@nestjs/config';
-import { validationSchema } from '../assets/validation.schema';
-import appConfig from '../assets/app.config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import AppConfig from '../assets/app.config';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { BlogPostsModule } from '@blog-platform/blog-posts';
+import { CoreModule } from '@blog-platform/core';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-    load: [appConfig],
-    validationSchema: validationSchema
-  }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule.forFeature(appConfig)],
-      useFactory: (config: ConfigType<typeof AppConfig>) => ({
-        type: 'postgres',
-        host: config.database.host,
-        port: config.database.port,
-        username: config.database.username,
-        password: config.database.password,
-        database: config.database.name,
-        autoLoadEntities: true,
-        synchronize: config.database.synchronize
-      }),
-      inject: [AppConfig.KEY]
-    }),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: true,
-      installSubscriptionHandlers: true,
-    }),
-    BlogPostsModule
-  ],
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [CoreModule, BlogPostsModule],
 })
 export class AppModule {
 }

b0bb2490e89add2a1a5b5fbad01ec8f2d8584592
iyed <73111142+Iyed73@users.noreply.github.com>
2025-05-09 23:16:29 +0100

    Create LoggerModule with transient scope custom logger

--- libs/common/src/lib/common.module.ts
+++ libs/common/src/lib/common.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PubSubModule } from './pub-sub/pub-sub.module';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
-  imports: [PubSubModule],
+  imports: [PubSubModule, LoggerModule],
   controllers: [],
   providers: [],
   exports: [PubSubModule],

d207ece92e4a985155f9205f5074290cc8545f52
iyed <73111142+Iyed73@users.noreply.github.com>
2025-05-11 12:36:20 +0100

    Change custom logger to provider

--- libs/common/src/lib/logger/logger.module.ts
+++ libs/common/src/lib/logger/logger.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { CustomLogger } from './custom-logger.service';
+import { CustomLogger } from './custom-logger.provider';
 
 @Module({
   providers: [CustomLogger],
